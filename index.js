de4be828822054d60de8f163809c324570175ce4
Wayne <wayne@u.northwestern.edu>
2017-04-23 21:13:26 -0500

    second try, basic working bot

--- index.js
+++ index.js
@@ -1,86 +1,66 @@
-const http = require('http')
-const Bot = require('messenger-bot')
+'use strict'
 
-let bot = new Bot({
-  token: 'PAGE_TOKEN',
-  verify: 'VERIFY_TOKEN',
-  app_secret: 'APP_SECRET'
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+	res.send('Hello world, I am a chat bot')
 })
 
-bot.on('error', (err) => {
-  console.log(err.message)
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Error, wrong token')
 })
 
-bot.on('message', (payload, reply) => {
-  let text = payload.message.text
+// Spin up the server
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'))
+})
 
-  bot.getProfile(payload.sender.id, (err, profile) => {
-    if (err) throw err
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+	    let event = req.body.entry[0].messaging[i]
+	    let sender = event.sender.id
+	    if (event.message && event.message.text) {
+		    let text = event.message.text
+		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+	    }
+    }
+    res.sendStatus(200)
+})
 
-    reply({ text }, (err) => {
-      if (err) throw err
+const token = "<PAGE_ACCESS_TOKEN>";
 
-      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+		json: {
+		    recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+		    console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
     })
-  })
-})
-
-// http.createServer(bot.middleware()).listen(3000)
-// console.log('Echo bot server running at port 3000.')
-
-// const Bot = require('fb-local-chat-bot');
-// const express = require('express');
-// const bodyParser = require('body-parser')
-// const Promise = require('bluebird');
-// const getLocation = require('./nlp.js').getLocation;
-// const getCase = require('./nlp.js').getCase;
-// const cases = require('./cases.js').loadCases();
-
-// // initialize Bot and define event handlers
-// Bot.init('<TOKEN>', '<VERIFY_TOKEN>', true /*useLocalChat*/, false /*useMessenger*/);
-
-// Bot.on('text', (event) => {
-// 	// extract some parameters
-// 	const senderID = event.sender.id;
-// 	const text = event.message.text;
-
-// 	// get case
-// 	var location = getLocation(text);
-// 	var caseNode = getCase(text);
-// 	// perform action
-// 	messageBody = cases[caseNode].action();
-
-// 	// send message
-// 	Bot.sendText(senderID, "Location: " + JSON.stringify(location, null, 4));
-// 	Bot.sendText(senderID, "case: " + caseNode);
-// 	Bot.sendText(senderID, "messageBody: " + messageBody);
-
-
-// });
-
-
-// const app = express();
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-
-// app.use('/chat', Bot.router());
-
-// var server = app.listen((process.env.PORT || 8080));
-// return server;
-
-
-// Bot.on('text', async (event: object) => {
-//   // do something
-// });
-
-// Bot.on('attachments', async (event: object) => {
-//   // do something
-// });
-
-// Bot.on('postback', async (event: object) => {
-//   // do something
-// });
-
-// app.use('/webhook', Bot.router());
-// // go to http://localhost:5000/webhook/localChat/ for local chat debugging
+}
